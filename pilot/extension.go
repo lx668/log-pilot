1a7924296dc767774efd4bdf8243d31b9f02712b
admin <lixiang@kanzhun.com>
2020-08-04 20:25:58 +0800

    1.基础镜像增加curl指令 2.根据node进入多kafka集群 2.extension增加env环境变量，作为labels标签被filebeat采集，最后作为应用区分搜索标记elasticsearch

--- pilot/extension.go
+++ pilot/extension.go
@@ -1,6 +1,7 @@
 package pilot
 
 import (
+	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/api/types"
 	"strings"
 )
@@ -13,5 +14,27 @@ func extension(container map[string]string, containerJSON *types.ContainerJSON) 
 			name = strings.Replace(name, ".", "_", -1)
 			putIfNotEmpty(container, name, value)
 		}
+	}
+	env := containerJSON.Config.Env
+	log.Infof("55555 %s", env)
+	//containerEnvMap := make(map[string]string) // all container envs map
+	containerMetaEnvMap := make(map[string]string) // container meta env which will be injected into log-pilot
+	for _, e := range env {
+		envKV := strings.SplitN(e, "=", 2)
+		//containerEnvMap[envKV[0]] = envKV[1]
+		if strings.HasPrefix(e, "COM_ALIYUN_META_ENVS_") {
+			// e.g. COM_ALIYUN_META_ENVS_MY_POD_IP = k8s_pod_ip
+			// MY_POD_IP is the original env key,
+			// k8s_pod_ip is the target field name which put into the tplt.
+			log.Infof("777777 %v", e)
+			metaEnvName := strings.TrimPrefix(envKV[0], "COM_ALIYUN_META_ENVS_")
+			containerMetaEnvMap[metaEnvName] = envKV[1]
+		}
+	}
+	for k, v := range containerMetaEnvMap {
+		//if envValue, exists := containerEnvMap[metaEnvKey]; exists {
+		log.Infof("6666666 %s, %s", k, v)
+		putIfNotEmpty(container, k, v)
+		//}
 	}
 }
